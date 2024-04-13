0d94598b970ce2c598ffbc0d9c24654b0eec6b96
Steven Desk <steven.adm@hotmail.com>
2024-04-13 18:24:43 -0300

    Incremento e correções de entradas do usuário inválidas.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-![Definições](/img/img1.jpg)
-![Expressões Regulares](/img/img2.jpg)
-![Cenário Exemplo](/img/img3.jpg)
+# Aplicação de Criptografia e Manipulação de Coordenadas Geográficas
+
+## Opção 1 - Informar posição:
+- O usuário pode inserir coordenadas geográficas (latitude e longitude), que são convertidas em algarismos romanos.
+- As coordenadas convertidas são então criptografadas usando uma chave de criptografia gerada aleatoriamente.
+- A chave de criptografia e a mensagem criptografada são exportadas para arquivos.
+
+## Opção 2 - Enviar mensagem:
+- O usuário pode inserir uma mensagem, que é criptografada usando uma chave de criptografia gerada aleatoriamente.
+- A chave de criptografia e a mensagem criptografada são exportadas para arquivos.
+
+## Opção 3 - Decriptar mensagem:
+- O usuário fornece uma mensagem criptografada e a chave de criptografia correspondente.
+- A mensagem é descriptografada usando a chave fornecida e exibida no console.
+
+Essencialmente, o programa permite ao usuário criptografar e descriptografar mensagens,
+bem como manipular coordenadas geográficas e armazenar informações criptografadas em arquivos para uso posterior.
--- criptografia.py
+++ criptografia.py
@@ -2,36 +2,16 @@ from cryptography.fernet import Fernet
 import os
 
 
-# Este programa é uma aplicação simples de criptografia e manipulação de coordenadas geográficas.
-
-# Opção 1 - Informar posição:
-#   - O usuário pode inserir coordenadas geográficas (latitude e longitude), que são convertidas em algarismos romanos.
-#   - As coordenadas convertidas são então criptografadas usando uma chave de criptografia gerada aleatoriamente.
-#   - A chave de criptografia e a mensagem criptografada são exportadas para arquivos.
-
-# Opção 2 - Enviar mensagem:
-#   - O usuário pode inserir uma mensagem, que é criptografada usando uma chave de criptografia gerada aleatoriamente.
-#   - A chave de criptografia e a mensagem criptografada são exportadas para arquivos.
-
-# Opção 3 - Decriptar mensagem:
-#   - O usuário fornece uma mensagem criptografada e a chave de criptografia correspondente.
-#   - A mensagem é descriptografada usando a chave fornecida e exibida no console.
-
-# Essencialmente, o programa permite ao usuário criptografar e descriptografar mensagens,
-# bem como manipular coordenadas geográficas e armazenar informações criptografadas em arquivos para uso posterior.
-
-
 def limparTela():
     os.system('cls')
 
 def exportarTextoChave(txt):
-    with open("chave.txt", "wb") as arquivo:  
-        #texto = txt.decode('utf-8')
+    with open("./arquivos/chave.txt", "wb") as arquivo:
         arquivo.write(txt)
         arquivo.close()
 
 def exportarTextoMensagem(string):
-    with open('./mensagem.txt', 'wb') as file:
+    with open('./arquivos/mensagem.txt', 'wb') as file:
         file.write(string)
 
 def exibirChave(key):
@@ -40,8 +20,8 @@ def exibirChave(key):
 def exibirMensagem(mensagem):
     print(mensagem)
 
-def inteiroParaRomano(graus):
-    numeros_romanos = {
+def inteiroParaRomano(numeroOriginal):
+    numerosRomanos = {
         100: 'C',
         90: 'XC',
         50: 'L',
@@ -52,126 +32,158 @@ def inteiroParaRomano(graus):
         4: 'IV',
         1: 'I'
     }
-    numero_convertido = ''
-    for value, representacao in numeros_romanos.items():
-        while graus >= value:
-            numero_convertido += representacao
-            graus -= value
-    return numero_convertido
+
+    numeroConvertido = ''
+    for valor, representacao in numerosRomanos.items():
+        while numeroOriginal >= valor:
+            numeroConvertido += representacao
+            numeroOriginal -= valor
+
+    return numeroConvertido
 
 def informarPosicoes():
+    
     feed = True
     while feed:
-        latitude = input('Insira a latitude(N ou S): ')
-        feed = validarLatitude(latitude)
+        direcaoLatitude = input('Insira a latitude (N ou S): ')
+        feed = validarLatitude(direcaoLatitude)
+    
     feed = True
     while feed:
-        grau_latitude = int(input('Informe o grau: '))
-        feed = validarGrauLatitude(grau_latitude)
+        grauLatitude = int(input('Informe o grau: '))
+        feed = validarGrauLatitude(grauLatitude)
+    
     feed = True
     while feed:
-        minutos_latitude = int(input('Informe os minutos latitude: '))
-        feed = validarMinutoOuSegundo(minutos_latitude)
+        minutosLatitude = int(input('Informe os minutos latitude: '))
+        feed = validarMinutoOuSegundo(minutosLatitude)
+    
     feed = True
     while feed:
-        segundos_latitude = int(input('Informe os segundos latitude: '))
-        feed = validarMinutoOuSegundo(segundos_latitude)
+        segundosLatitude = int(input('Informe os segundos latitude: '))
+        feed = validarMinutoOuSegundo(segundosLatitude)
+    
     feed = True
     while feed:
-        longitude = input('Insira a longitude(O ou L): ')
-        feed = validarLogitude(longitude)
+        direcaoLongitude = input('Insira a longitude (O ou L): ')
+        feed = validarLogitude(direcaoLongitude)
+    
     feed = True
     while feed:
-        grau_longitude = int(input('Informe o grau: '))
-        feed = validarGrauLongitude(grau_longitude)
+        grauLongitude = int(input('Informe o grau: '))
+        feed = validarGrauLongitude(grauLongitude)
+    
     feed = True
     while feed:
-        minutos_longitude = int(input('Informe os minutos longitude: '))
-        feed = validarMinutoOuSegundo(minutos_longitude)
+        minutosLongitude = int(input('Informe os minutos longitude: '))
+        feed = validarMinutoOuSegundo(minutosLongitude)
+    
     feed = True
     while feed:
-        segundos_longitude = int(input('Informe os segundos longitude: '))
-        feed = validarMinutoOuSegundo(segundos_longitude)
+        segundosLongitude = int(input('Informe os segundos longitude: '))
+        feed = validarMinutoOuSegundo(segundosLongitude)
+    
+    direcaoLatitude = 'a' if direcaoLatitude.lower() == 'n' else 'b'
+    direcaoLongitude = 'x' if direcaoLongitude.lower() == 'o' else 'y'
+    
     coordenada = [
-                    latitude,
-                        inteiroParaRomano(grau_latitude),
-                        inteiroParaRomano(minutos_latitude),
-                        inteiroParaRomano(segundos_latitude),
-                    longitude,
-                        inteiroParaRomano(grau_longitude),
-                        inteiroParaRomano(minutos_longitude),
-                        inteiroParaRomano(segundos_longitude)
+                    direcaoLatitude,
+                        inteiroParaRomano(grauLatitude),
+                        inteiroParaRomano(minutosLatitude),
+                        inteiroParaRomano(segundosLatitude),
+                    direcaoLongitude,
+                        inteiroParaRomano(grauLongitude),
+                        inteiroParaRomano(minutosLongitude),
+                        inteiroParaRomano(segundosLongitude)
                 ]
+    
     coordenadaOriginal = ' '.join(coordenada)
-    chave = generate_key()
+    
+    chave = gerarChave()
+    
     coordenadaEncriptada = encrypt(coordenadaOriginal, chave)
+    
     exportarTextoChave(chave)
     exportarTextoMensagem(coordenadaEncriptada)
+    
     return True
 
-def validarLatitude(latitude):
-    if latitude == 'S' or latitude == 's' or latitude == 'N' or latitude == 'n':
+def validarLatitude(direcaolatitude):
+    if direcaolatitude.lower() in ['n', 's']:
         return False
     else:
         limparTela()
-        print('Latitude invalida, tente novamente.')
+        print('\nLatitude invalida, tente novamente.\n')
         return True
     
-def validarLogitude(latitude):
-    if latitude == 'O' or latitude == 'o' or latitude == 'L' or latitude == 'l':
+def validarLogitude(direcaoLatitude):
+    if direcaoLatitude.lower() in ['o', 'l']:
         return False
     else:
         limparTela()
-        print('Logitude invalida, tente novamente.')
+        print('\nLogitude invalida, tente novamente.\n')
         return True
 
-def validarGrauLatitude(num):
-    if num <= 90:
-        return False
-    else:
+def validarGrauLatitude(numeroGrauLatitude):
+    try:
+        if 0 <= numeroGrauLatitude <= 90:
+            return False
+        else:
+            limparTela()
+            print('\nGrau informado e invalido, tente novamente.\n')
+            return True
+    except ValueError:
         limparTela()
-        print('Grau informado e invalido, tente novamente.')
+        print('\nGrau para Latitude informada é inválida. Certifique-se de inserir um número inteiro.\n')
         return True
 
-def validarGrauLongitude(num):
-    if num <= 180:
-        return False
-    else:
+def validarGrauLongitude(numeroGrauLongitude):
+    try:
+        if 0 <= numeroGrauLongitude <= 180:
+            return False
+        else:
+            limparTela()
+            print('\nGrau informado e invalido, tente novamente.\n')
+            return True
+    except ValueError:
         limparTela()
-        print('Grau informado e invalido, tente novamente.')
+        print('\nGrau para Logitude informada é inválida. Certifique-se de inserir um número inteiro.\n')
         return True
     
-def validarMinutoOuSegundo(num):
-    if num < 60:
-        return False
-    else:
+def validarMinutoOuSegundo(numeroMinutoOuSegundo):
+    try:
+        if 0 <= numeroMinutoOuSegundo < 60:
+            return False
+        else:
+            limparTela()
+            print('\nMinuto ou Segundo informado e invalido, tente novamente\n')
+            return True
+    except ValueError:
         limparTela()
-        print('Minuto ou Segundo informado e invalido, tente novamente')
+        print('\nValor informado para minuto ou segundo é inválido. Certifique-se de inserir um número inteiro.\n')
         return True
 
 
 def informarMensagem():
     textoOriginal = input('Texto: ')
-    chave = generate_key()
+    chave = gerarChave()
     textoCifrado = encrypt(textoOriginal, chave)
-    #exibirChave(chave)
-    #exibirMensagem(textoCifrado)
     exportarTextoChave(chave)
     exportarTextoMensagem(textoCifrado)
 
-def generate_key():
+def gerarChave():
     return Fernet.generate_key()
 
-def encrypt(message, key):
-    fernet = Fernet(key)
-    encrypted_message = fernet.encrypt(message.encode())
-    return encrypted_message
+def encrypt(textoClaro, chave):
+    fernet = Fernet(chave)
+    textoCifrado = fernet.encrypt(textoClaro.encode())
+    return textoCifrado
 
-def decrypt(encrypted_message, key):
+def decrypt(mensagemEncriptada, chave):
     try:
-        fernet = Fernet(key)
-        decrypted_message = fernet.decrypt(encrypted_message)
-        return decrypted_message.decode()
+        fernet = Fernet(chave)
+        textoClaro = fernet.decrypt(mensagemEncriptada)
+        return textoClaro.decode()
     except InvalidToken:
         print("Erro: Chave de descriptografia inválida.")
         return None
@@ -179,44 +191,59 @@ def decrypt(encrypted_message, key):
 def main():
     
     limparTela()
-    
+
     while True:
         
-        print("Informe a opção desejada:")
+        print("Informe o número da opção desejada:")
         print("1 - Informar posição")
         print("2 - Enviar mensagem")
         print("3 - Decriptar mensagem")
         print("\n0 - Encerrar programa")
 
-        opcao = int(input("> "))
+        try:
 
-        if opcao == 1:
-            print("Opção 1 selecionada. Informando posição...")
-            informar_posicoes()
-            print("Informações geradas com sucesso.")
-            
-        elif opcao == 2:
-            print("Opção 2 selecionada. Enviando mensagem...")
-            informar_mensagem()
-            print("Mensagem enviada com sucesso.")
-            
-        elif opcao == 3:
-            print("Opção 3 selecionada. Decriptando mensagem...")
-            mensagem_criptografada = input('Informe a mensagem criptografada: ')
-            chave = input('Informe a chave de criptografia: ')
-            mensagem_decriptada = decrypt(mensagem_criptografada.encode(), chave.encode())
-            
-            if mensagem_decriptada is not None:
+            opcao = int(input("> "))
+
+            if opcao == 1:
+
                 limparTela()
-                print(f'\nMensagem decriptada:\n{mensagem_decriptada}\n')
-                
-        elif opcao == 0:
-            print("Programa finalizado!")
-            break
-        
-        else:
-            print("Opção inválida. Por favor, escolha uma opção válida.")
 
+                print("Opção 1 selecionada. Informando posição...")
+                informarPosicoes()
+                print("\nInformações geradas com sucesso.\n")
+                
+            elif opcao == 2:
+
+                limparTela()
+
+                print("Opção 2 selecionada. Enviando mensagem...")
+                informarMensagem()
+
+                print("\nMensagem enviada com sucesso.\n")
+                
+            elif opcao == 3:
+
+                limparTela()
+
+                print("Opção 3 selecionada. Decriptando mensagem...")
+                mensagem_criptografada = input('\nInforme a mensagem criptografada: ')
+                
+                chave = input('\nInforme a chave de criptografia: ')
+                mensagem_decriptada = decrypt(mensagem_criptografada.encode(), chave.encode())
+                
+                if mensagem_decriptada is not None:
+                    limparTela()
+                    print(f'Mensagem decriptada:\n{mensagem_decriptada}\n')
+                    
+            elif opcao == 0:
+                print("\nPrograma finalizado!")
+                break
+            
+            else:
+                print("\nOpção inválida. Por favor, escolha um dos número no menu apresentado.\n")
+
+        except ValueError:
+            print("\nOpção informada não é um número, por favor insira apenas um número das opções apresentadas.\n")
 
 if __name__ == "__main__":
     main()
